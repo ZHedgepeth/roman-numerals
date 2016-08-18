669cab381ef63a4fd91649cf5c4eaca5ff391938
alexandre leibler and zachary hedgepeth <alexandre@>
2016-08-18 16:29:29 -0400

    add script and html to roman numeral web page

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,34 @@ $(document).ready(function() {
     baseTenValues.push(hundreds);
     baseTenValues.push(thousands);
 
+    var zeroSpecial = ["IX", "IV"];
+    var oneSpecial = ["XC", "XL"];
+    var twoSpecial = ["CM", "CD"];
+    var threeSpecial = ["MMMMMMMMM" , "MMMM"];
+
+    var specialNumbers = [];
+    specialNumbers.push(zeroSpecial);
+    specialNumbers.push(oneSpecial);
+    specialNumbers.push(twoSpecial);
+    specialNumbers.push(threeSpecial);
+
+    // REGEX: ^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$
+    for (var baseIndex = 0; baseIndex < baseTenValues.length; baseIndex++) {
+      if (baseTenValues[baseIndex] === 9) {
+        var specialChunk = specialNumbers[baseIndex][0];
+        console.log(specialChunk);
+
+      }
+      else if (baseTenValues[baseIndex] === 4) {
+        var specialChunk = specialNumbers[baseIndex][1];
+        console.log(specialChunk);
+      }
+      else {
+        // console.log("do the thing zhu-li");
+
+      }
+    }
+
     console.log(baseTenValues);
   });
 });
